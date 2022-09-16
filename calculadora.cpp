2fb79da4530b062d305692392ec01379fbafb49d
Stain Francis <educaysoft@gmail.com>
2022-09-15 21:34:11 -0400

    se adiciono el programa calculadora.cpp y otros

---
+++ calculadora.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+float x,y,s,r,d,m;
+cout<<"Ingrese el valor de x="; cin>>x;
+cout<<"Ingrese el valor de y="; cin>>y;
+s=x+y;
+r=x-y;
+d=x/y;
+m=x*y;
+cout<<"El Resultado de la sumar "<<x<<" + "<<y<<" = "<<s<<endl;
+cout<<"El Resultado de la resta "<<x<<" - "<<y<<" = "<<r<<endl;
+cout<<"El Resultado de la division "<<x<<" / "<<y<<" = "<<d<<endl;
+cout<<"El Resultado de la multiplicación "<<x<<" * "<<y<<" = "<<m<<endl;
+
+return 0;
+
+
+}
