97c913cb67136d6294862eb5540dc5320a365ba4
martha <martha.loor.andrade@utelvt.edu.ec>
2022-09-24 22:14:18 +0000

    cambie las variables de contabilizacion

--- contabiliza.cpp
+++ contabiliza.cpp
@@ -1,20 +1,20 @@
-//Creado por: Martha Paola Loor Andrade
+//creado por: Martha Paola Loor Andrade
 #include<iostream>
 using namespace std;
 int main()
 {
-	int i=0,l;
-	float x,s=0;
-	cout<<"ingrese el limite l="; cin>>l;
+	int MPLA_i=0,MPLA_l;
+	float MPLA_x,MPLA_s=0;
+	cout<<"ingrese el limite MPLA_l="; cin>>MPLA_l;
 	do{
 
-	cout<<"ingrese el numero x="; cin>>x;
-	i=i+1;
-	s=s+x;
+	cout<<"ingrese el numero MPLA_x="; cin>>MPLA_x;
+	MPLA_i=MPLA_i+1;
+	MPLA_s=MPLA_s+MPLA_x;
 
 
 
-	}while(i<l);
-	cout<<"Se ingresaron "<<l<<" numeros "<< "que sumaron "<<s<<endl;
+	}while(MPLA_i<MPLA_l);
+	cout<<"Se ingresaron "<<MPLA_l<<" numeros "<< "que sumaron "<<MPLA_s<<endl;
 	return 0;
 }
