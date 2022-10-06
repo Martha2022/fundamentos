31af6f3900e9eb0ebdf465f92216e59a4142744d
yo soy martha <martha.loor.andrade@utelvt.edu.ec>
2022-10-06 08:59:42 -0500

    mejoras

---
+++ suma.cpp
@@ -0,0 +1,17 @@
+//Creado por Martha Paola Loor Andrade
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+ float mpla_x,mpla_y,mpla_z;
+ cout<<"Ingrese 2 numero mpla_x mpla_y :";
+ cin>>mpla_x>>mpla_y;
+ mpla_z=mpla_x+mpla_y;
+ cout<<"El resutado es : ";
+ cout<<mpla_z<<endl;
+
+ return 0;
+
+}
